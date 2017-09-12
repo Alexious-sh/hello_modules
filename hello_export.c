8ec23d05f7202ce637b9bc45222f48ea703d9329
Oleksii Shavykin <oleksii.shavykin@globallogic.com>
2017-09-12 14:40:29 +0300

    Codestyle fixes

--- hello_export.c
+++ hello_export.c
@@ -8,18 +8,18 @@ static int __init hello_init(void)
 	return 0;
 }
 
-int print_hello(void) {
-	printk(KERN_EMERG "Hello, world!\n");
+int print_hello(void)
+{
+	pr_emerg("Hello, world!\n");
 	return 0;
-
 }
+EXPORT_SYMBOL(print_hello);
 
 static void __exit hello_exit(void)
 {
 	/* Do nothing here right now */
 }
 
-EXPORT_SYMBOL(print_hello);
 module_init(hello_init);
 module_exit(hello_exit);
 
